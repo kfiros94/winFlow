44a9833b519f71a3dee4659f3f37b77f0e52e94f
Kfir Amoyal <kfiramoyal@gmail.com>
2026-04-01 16:27:28 +0300

    add SportMatch controller

---
+++ winflow/winflow/src/main/java/com/winflow/winflow/controller/SportMatchController.java
@@ -0,0 +1,28 @@
+package com.winflow.winflow.controller;
+
+import com.winflow.winflow.entity.SportMatch;
+import com.winflow.winflow.service.SportMatchService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // Tells Spring: "This class listens for internet traffic and replies with JSON"
+@RequestMapping("/api/matches") // The base URL for everything in this class
+public class SportMatchController {
+    private final SportMatchService matchService;
+
+    // Standard Java Constructor to inject the service we built earlier
+    public SportMatchController(SportMatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    /**
+     * When a browser goes to http://localhost:8080/api/matches, this method fires!
+     */
+    @GetMapping
+    public List<SportMatch> getAllAvailableMatches() {
+        return matchService.getAvailableMatches();
+    }
+}
